1ea31944e534b9634738b2abcd04e0d05ef738d0
Pavlechko <pavlechko.serhii@gmail.com>
2022-02-06 20:14:51 +0200

    modified publisher router, added authorization

--- routes/publishers-router.ts
+++ routes/publishers-router.ts
@@ -1,7 +1,9 @@
 import Router from "express";
+
 import publishersControllers from "../controllers/publishers-controllers";
+import checkRoleMiddleware from "../middleware/checkRoleMiddleware";
 
 export const router = Router();
 
-router.post('/', publishersControllers.create);
+router.post('/', checkRoleMiddleware('Admin'), publishersControllers.create);
 router.get('/', publishersControllers.getAll);
