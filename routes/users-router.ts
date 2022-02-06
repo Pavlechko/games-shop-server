d140366ebfa41c682a044564ba810bd708dfa782
Pavlechko <pavlechko.serhii@gmail.com>
2022-02-06 20:15:11 +0200

    modified user router, added authorization

--- routes/users-router.ts
+++ routes/users-router.ts
@@ -1,12 +1,14 @@
 import Router from "express";
 
 import usersControllers from "../controllers/users-controllers";
+import authMiddleware from "../middleware/authMiddleware";
 
 const usersRouter = Router();
 
 usersRouter.post('/registration', usersControllers.registration);
 usersRouter.post('/login', usersControllers.login);
-usersRouter.get('/auth', usersControllers.check);
+usersRouter.get('/auth', authMiddleware, usersControllers.check);
 usersRouter.delete('/:id', usersControllers.delete);
+usersRouter.get('/', usersControllers.getAll);
 
 export default usersRouter;
