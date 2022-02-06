5c7eb39128fb72b57c9f6962b245a305c901b2f0
Pavlechko <pavlechko.serhii@gmail.com>
2022-02-06 20:14:25 +0200

    modified genres router added authorization

--- routes/genres-router.ts
+++ routes/genres-router.ts
@@ -1,9 +1,11 @@
 import Router from "express";
+
 import genresControllers from "../controllers/genres-controllers";
+import checkRoleMiddleware from "../middleware/checkRoleMiddleware";
 
 export const router = Router();
 
 
-router.post('/', genresControllers.create);
+router.post('/', checkRoleMiddleware('Admin'), genresControllers.create);
 
 router.get('/', genresControllers.getAll);
