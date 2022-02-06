7ccfeaf435326423487759a97d54b874d32f1fdb
Pavlechko <pavlechko.serhii@gmail.com>
2022-02-06 20:14:05 +0200

    modified games router added authorization

--- routes/games-router.ts
+++ routes/games-router.ts
@@ -1,9 +1,10 @@
 import Router from "express";
 import gamesControllers from "../controllers/games-controllers";
+import checkRoleMiddleware from "../middleware/checkRoleMiddleware";
 
 export const router = Router();
 
-router.post('/', gamesControllers.create);
+router.post('/', checkRoleMiddleware('Admin'), gamesControllers.create);
 router.get('/', gamesControllers.getAll);
 router.get('/:id', gamesControllers.getOne);
-router.delete('/:id', gamesControllers.delete);
+router.delete('/:id', checkRoleMiddleware('Admin'), gamesControllers.delete);
